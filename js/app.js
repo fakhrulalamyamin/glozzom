fc81923c4998a2bf4177cec5dc716ee5c3e00064
fakhrulalamyamin <fakhrulalam.yamin143245@gmail.com>
2020-08-29 19:35:51 +0600

    finished index page

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
-$('#year').text(new Date().getFullYear());
+$("#year").text(new Date().getFullYear());
 
-$('.carousel').carousel({
-    interval: 6000,
-    pause: 'hover'
+$(".carousel").carousel({
+  interval: 6000,
+  pause: "hover",
 });
 
+// ekko lightbox
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+// video play
 $(function () {
-    // Auto play modal video
-    $(".video").click(function () {
-      var theModal = $(this).data("target"),
-        videoSRC = $(this).attr("data-video"),
-        videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
-      $(theModal + ' iframe').attr('src', videoSRCauto);
-      $(theModal + ' button.close').click(function () {
-        $(theModal + ' iframe').attr('src', videoSRC);
-      });
+  // Auto play modal video
+  $(".video").click(function () {
+    var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto =
+        videoSRC +
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + " iframe").attr("src", videoSRCauto);
+    $(theModal + " button.close").click(function () {
+      $(theModal + " iframe").attr("src", videoSRC);
     });
   });
+});
